63734acb0ad5541d56859180c8d30b25816ff809
Daniel <daniel.lub2007@gmail.com>
2024-04-16 18:28:46 +0300

    6

--- app/src/main/java/com/example/quadraticequationdaniellublinsky/MainActivity2.java
+++ app/src/main/java/com/example/quadraticequationdaniellublinsky/MainActivity2.java
@@ -44,8 +44,15 @@ public class MainActivity2 extends AppCompatActivity
         ans = findViewById(R.id.ans);
         double ans1;
         double ans2;
+        double temp;
         ans1 = (-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
         ans2 = (-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
+        if(ans1 > ans2)
+        {
+            temp = ans1;
+            ans1 = ans2;
+            ans2 = temp;
+        }
 
         if(ans1 == ans2) // same answer
         {
