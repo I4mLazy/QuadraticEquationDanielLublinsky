23e665bc65bc14b5c46290b7bebb8827b2176f02
Daniel <daniel.lub2007@gmail.com>
2024-04-16 17:13:14 +0300

    4

--- app/src/main/java/com/example/quadraticequationdaniellublinsky/MainActivity2.java
+++ app/src/main/java/com/example/quadraticequationdaniellublinsky/MainActivity2.java
@@ -9,10 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.widget.TextView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity2 extends AppCompatActivity
 {
     TextView ans;
     Button ret;
+    LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +34,8 @@ public class MainActivity2 extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lineChart = findViewById(R.id.line_chart);
+
         Intent gi = getIntent();
         ret = findViewById(R.id.ret);
         int a = Integer.parseInt(gi.getStringExtra("a"));
@@ -35,29 +46,71 @@ public class MainActivity2 extends AppCompatActivity
         double ans2;
         ans1 = (-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
         ans2 = (-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
+
         if(ans1 == ans2) // same answer
         {
             ans.setText(String.valueOf(ans1));
+            List<Entry> entries = new ArrayList<>();
+            for (float x = (float) ans1-10; x <= (float) ans1+10; x += 0.1) {
+                float y = a * x * x + b * x + c;
+                entries.add(new Entry(x, y));
+            }
+            LineDataSet dataSet = new LineDataSet(entries, "Function: " + a + "x² + " + b + "x + " + c);
+            LineData lineData = new LineData(dataSet);
+            lineChart.setData(lineData);
         }
         else if(Double.isNaN(ans1)) // ans1 is NaN
         {
             if(Double.isNaN(ans2)) //both are NaN
             {
                 ans.setText("No Answers");
+                List<Entry> entries = new ArrayList<>();
+                for (float x = -10; x <= 10; x += 0.1) {
+                    float y = a * x * x + b * x + c;
+                    entries.add(new Entry(x, y));
+                }
+                LineDataSet dataSet = new LineDataSet(entries, "Function: " + a + "x² + " + b + "x + " + c);
+                LineData lineData = new LineData(dataSet);
+                lineChart.setData(lineData);
             }
             else //ans2 is the only answer
             {
                 ans.setText(String.valueOf(ans2));
+                List<Entry> entries = new ArrayList<>();
+                for (float x = (float) ans2-10; x <= (float) ans2 + 10; x += 0.1) {
+                    float y = a * x * x + b * x + c;
+                    entries.add(new Entry(x, y));
+                }
+                LineDataSet dataSet = new LineDataSet(entries, "Function: " + a + "x² + " + b + "x + " + c);
+                LineData lineData = new LineData(dataSet);
+                lineChart.setData(lineData);
             }
         }
         else if(Double.isNaN(ans2)) //ans1 is the only answer
         {
             ans.setText(String.valueOf(ans1));
+            List<Entry> entries = new ArrayList<>();
+            for (float x = (float) ans1-10; x <= (float) ans1 + 10; x += 0.1) {
+                float y = a * x * x + b * x + c;
+                entries.add(new Entry(x, y));
+            }
+            LineDataSet dataSet = new LineDataSet(entries, "Function: " + a + "x² + " + b + "x + " + c);
+            LineData lineData = new LineData(dataSet);
+            lineChart.setData(lineData);
         }
         else // two answers
         {
             ans.setText(String.valueOf(ans1) + ", " + String.valueOf(ans2));
+            List<Entry> entries = new ArrayList<>();
+            for (float x = (float) ans1-10; x <= (float) ans2 + 10; x += 0.1) {
+                float y = a * x * x + b * x + c;
+                entries.add(new Entry(x, y));
+            }
+            LineDataSet dataSet = new LineDataSet(entries, "Function: " + a + "x² + " + b + "x + " + c);
+            LineData lineData = new LineData(dataSet);
+            lineChart.setData(lineData);
         }
+
     }
 
     public void ret(View view)
